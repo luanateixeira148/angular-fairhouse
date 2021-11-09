fb8ec760be3e4c7cfc64e7614373b019c9782fe8
Luana <luanateixeira148@gmail.com>
2021-11-09 11:01:21 -0800

    creates details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '../location';
+import { LocationService } from '../location.service';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private locationService: LocationService) { }
 
-  constructor() { }
+  selectedLocationId: number = -1;
+  location: Location | undefined;
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.selectedLocationId = +params['id'];
+      this.location = this.locationService.getLocationById(this.selectedLocationId);
+    });
   }
 
 }
